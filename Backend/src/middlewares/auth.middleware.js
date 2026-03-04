986cd73848a8d2a79d066ace10496b4776ace577
OmprakashMaurya16 <omprakash.maurya1604@gmail.com>
2026-03-04 20:52:56 +0530

    Auth middleware & JWT

---
+++ Backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken";
+import ApiError from "../utils/api.error.js";
+import asyncHandler from "../utils/async.handler.js";
+import User from "../models/user.model.js";
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  let decoded;
+
+  try {
+    decode = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    throw new ApiError(401, "Invalid token");
+  }
+
+  const user = await User.findById(decoded.id);
+
+  if (!user || !user.isActive) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  req.user = {
+    id: user._id,
+    role: user.role,
+  };
+
+  next();
+});
+
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new ApiError(403, "Forbidden");
+    }
+    next();
+  };
+};
+
+export { authMiddleware, authorizeRoles };
---
+++ Backend/src/utils/token.util.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+const generateAccessToken = (user) => {
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "1m",
+  });
+};
+
+const generateRefreshToken = (user) => {
+  return jwt.sign({ id: user._id }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+export { generateAccessToken, generateRefreshToken };
